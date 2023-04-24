5993579cb3c909aa14088b5a90f7758e7fdce56e
DE-corp <crazyHOPHOP@rambler.ru>
2023-04-24 21:55:51 +0300

    Добавьте файлы проекта.

---
+++ Module.17.Patterns.Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module._17.Patterns.Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
